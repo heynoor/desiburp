c50def30d8ff79795775707835614cedcf26050d
heynoor <heynoor@gmail.com>
2014-04-23 00:54:34 +0800

    learning deligate in objective c

--- desiburp/ViewController.m
+++ desiburp/ViewController.m
@@ -9,10 +9,28 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *simpleTextField;
+@property (weak, nonatomic) IBOutlet UILabel *simpleLabel;
 @end
 
 @implementation ViewController
+
+- (IBAction)changeLabel:(id)sender {
+    //
+    NSString *contents = [[ self simpleTextField ] text ];
+    NSString *message = [ NSString stringWithFormat:@"Hello, %@", contents];
+    [[self simpleLabel] setText:message];
+    [[self simpleTextField] resignFirstResponder];
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [[self simpleTextField] resignFirstResponder];
+    return YES;
+}
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [[self view] endEditing:YES];
+}
 
 - (void)viewDidLoad
 {
